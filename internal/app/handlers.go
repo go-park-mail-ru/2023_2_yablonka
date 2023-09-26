9c8de33bb612802e09cae7e807030ad4157cc7c9
bqback <lolwut-lol@yandex.ru>
2023-09-26 21:14:15 +0300

    Setting up handler data, handler ideas

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -11,8 +11,9 @@ import (
 )
 
 type Handlers struct {
-	Users []datatypes.User
-	Mu    *sync.Mutex
+	Users  []datatypes.User
+	Boards []datatypes.Board
+	Mu     *sync.Mutex
 }
 
 func (h *Handlers) HandleSignupUser(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,7 @@ func (h *Handlers) HandleSignupUser(w http.ResponseWriter, r *http.Request) {
 	h.Mu.Lock()
 
 	var id uint64 = 0
+	// Сначала проверить почту на существование
 	if len(h.Users) > 0 {
 		id = h.Users[len(h.Users)-1].ID + 1
 	}
@@ -45,14 +47,45 @@ func (h *Handlers) HandleSignupUser(w http.ResponseWriter, r *http.Request) {
 	h.Mu.Unlock()
 }
 
-func (h *Handlers) HandleListUsers(w http.ResponseWriter, r *http.Request) {
+func (h *Handlers) HandleLoginUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+
+	newUserInput := new(datatypes.SignupInfo)
+	err := decoder.Decode(newUserInput)
+	if err != nil {
+		log.Printf("error while unmarshalling JSON: %s", err)
+		w.Write([]byte("{}"))
+		return
+	}
+
+	fmt.Println(newUserInput)
+	h.Mu.Lock()
+	// do stuff
+	h.Mu.Unlock()
+}
+
+func (h *Handlers) HandleGetBoards(w http.ResponseWriter, r *http.Request) {
 	encoder := json.NewEncoder(w)
 	h.Mu.Lock()
-	err := encoder.Encode(h.Users)
+	err := encoder.Encode(h.Boards)
 	h.Mu.Unlock()
 	if err != nil {
 		log.Printf("error while marshalling JSON: %s", err)
 		w.Write([]byte("{}"))
 		return
 	}
+}
+
+func (h *Handlers) HandleVerifyAuth(w http.ResponseWriter, r *http.Request) {
+	// encoder := json.NewEncoder(w)
+	// h.Mu.Lock()
+	// err := encoder.Encode(h.Boards)
+	// h.Mu.Unlock()
+	// if err != nil {
+	// 	log.Printf("error while marshalling JSON: %s", err)
+	// 	w.Write([]byte("{}"))
+	// 	return
+	// }
 }
