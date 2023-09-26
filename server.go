f0cbe6870812e79218daf07959760c46be4222b0
bqback <lolwut-lol@yandex.ru>
2023-09-26 21:14:03 +0300

    Setting up handler data

--- server.go
+++ server.go
@@ -11,23 +11,42 @@ import (
 
 func main() {
 	handlers := handlers.Handlers{
-		Users: make([]datatypes.User, 0),
-		Mu:    &sync.Mutex{},
+		Users:  make([]datatypes.User, 0),
+		Boards: make([]datatypes.Board, 0),
+		Mu:     &sync.Mutex{},
 	}
 
-	http.HandleFunc("/api/signup/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/login/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{}"))
+
+		log.Println(r.URL.Path)
+
+		if r.Method == http.MethodPost {
+			handlers.HandleLoginUser(w, r)
+			return
+		}
 	})
 
 	http.HandleFunc("/api/getboards/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{}"))
+
+		log.Println(r.URL.Path)
+
+		if r.Method == http.MethodGet {
+			handlers.HandleGetBoards(w, r)
+			return
+		}
 	})
 
 	http.HandleFunc("/api/verifyauth/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{}"))
+
+		log.Println(r.URL.Path)
+
+		if r.Method == http.MethodGet {
+			handlers.HandleVerifyAuth(w, r)
+			return
+		}
 	})
 
 	http.HandleFunc("/api/signup/", func(w http.ResponseWriter, r *http.Request) {
