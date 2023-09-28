b21363aefaa5bb98cfc5fc83b652ecf5bb7891be
Mike WInfield <lolwut-lol@yandex.ru>
2023-09-28 16:54:38 +0300

    Failed login tests

--- test/login_test.go
+++ test/login_test.go
@@ -17,14 +17,14 @@ func TestSuccess(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name          string
-		email         string
-		password_hash string
+		name         string
+		email        string
+		passwordHash string
 	}{
 		{
-			name:          "Existing entry",
-			email:         "test@email.com",
-			password_hash: "123456",
+			name:         "Existing entry",
+			email:        "test@email.com",
+			passwordHash: "123456",
 		},
 	}
 	for _, test := range tests {
@@ -34,7 +34,7 @@ func TestSuccess(t *testing.T) {
 			testApi := handlers.TestApi()
 
 			body := bytes.NewReader([]byte(
-				fmt.Sprintf(`{"email":"%s", "password_hash":"%s"}`, test.email, test.password_hash),
+				fmt.Sprintf(`{"email":"%s", "password_hash":"%s"}`, test.email, test.passwordHash),
 			))
 
 			r := httptest.NewRequest("POST", "/api/v1/login/", body)
@@ -44,7 +44,7 @@ func TestSuccess(t *testing.T) {
 
 			login := datatypes.LoginInfo{
 				Email:        test.email,
-				PasswordHash: test.password_hash,
+				PasswordHash: test.passwordHash,
 			}
 			expectedToken, err := testApi.GenerateJWT(login)
 
@@ -54,6 +54,48 @@ func TestSuccess(t *testing.T) {
 
 			require.NoError(t, err)
 			require.Contains(t, testApi.GetSessions(), expectedToken, "Expected token wasn't found")
+		})
+	}
+}
+
+func TestFail(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name          string
+		email         string
+		passwordHash  string
+		expectedError int
+	}{
+		{
+			name:          "User not found",
+			email:         "notfound@email.com",
+			passwordHash:  "123456",
+			expectedError: http.StatusNotFound,
+		},
+		{
+			name:          "Wrong password",
+			email:         "test@email.com",
+			passwordHash:  "654321",
+			expectedError: http.StatusUnauthorized,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			testApi := handlers.TestApi()
+
+			body := bytes.NewReader([]byte(
+				fmt.Sprintf(`{"email":"%s", "password_hash":"%s"}`, test.email, test.passwordHash),
+			))
+
+			r := httptest.NewRequest("POST", "/api/v1/login/", body)
+			w := httptest.NewRecorder()
+
+			testApi.HandleLoginUser(w, r)
+
+			require.EqualValues(t, w.Code, test.expectedError)
 		})
 	}
 }
