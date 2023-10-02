49f5261d70a87c8b6d166c56e52754cf195a788a
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2023-10-02 14:51:59 +0300

    Added wagger and some logs into main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,20 +6,34 @@ import (
 	"server/internal/app/config"
 )
 
+// @title Go + Gin Todo API
+// @version 1.0
+// @description This is a sample server todo server. You can visit the GitHub repository at https://github.com/LordGhostX/swag-gin-demo
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /
+// @query.collection.format multi
 func main() {
 	serverConfig, err := config.NewEnvConfig("")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	log.Println("server configured")
 
 	mux := http.NewServeMux()
-
 	err = config.ConfigMux(serverConfig, mux)
+	log.Println("mux configured")
 
 	http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal("Failed to start server")
 	}
 	// TODO graceful shutdown
-
 }
