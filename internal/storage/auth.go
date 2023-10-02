ee1f85aa8eb7e93c664fb66916a1110dfb128007
Mike WInfield <lolwut-lol@yandex.ru>
2023-10-02 06:13:03 +0300

    Change signature of DeleteSession to SSID

--- internal/storage/auth.go
+++ internal/storage/auth.go
@@ -8,5 +8,5 @@ import (
 type IAuthStorage interface {
 	CreateSession(context.Context, *entities.Session) (string, error)
 	GetSession(context.Context, string) (*entities.Session, error)
-	DeleteSession(context.Context, *entities.User) error
+	DeleteSession(context.Context, string) error
 }
