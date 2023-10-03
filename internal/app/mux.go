42b352e41befd92d6532eb41af942a27f2260c11
Mike WInfield <lolwut-lol@yandex.ru>
2023-10-03 20:48:35 +0300

    Добавить миддлвер для обработки ошибок

--- internal/app/mux.go
+++ internal/app/mux.go
@@ -16,18 +16,20 @@ import (
 // возвращает mux, реализованный с помощью модуля chi
 func GetChiMux(manager handlers.HandlerManager) (http.Handler, error) {
 	mux := chi.NewRouter()
-	mux.Use(middleware.Logger)
 	mux.Use(middleware.JsonHeader)
+	mux.Use(middleware.ErrorHandler)
+	mux.Use(middleware.Logger)
+	// mux.Use(middleware.PanicRecovery)
 
 	mux.Route("/api/v1/auth", func(r chi.Router) {
-		r.Post("/login", manager.AuthHandler.LogIn)
-		r.Post("/signup", manager.AuthHandler.SignUp)
-		r.Get("/verify", manager.AuthHandler.VerifyAuthEndpoint)
+		r.Post("/login/", manager.AuthHandler.LogIn)
+		r.Post("/signup/", manager.AuthHandler.SignUp)
+		r.Get("/verify/", manager.AuthHandler.VerifyAuthEndpoint)
 	})
 
 	mux.Route("/api/v1/user", func(r chi.Router) {
 		r.Use(manager.AuthHandler.VerifyAuthMiddleware)
-		r.Get("/boards", manager.BoardHandler.GetUserBoards)
+		r.Get("/boards/", manager.BoardHandler.GetUserBoards)
 	})
 
 	return mux, nil
