0311cc1549abb6860ebea497b0fa293aa0a2d26f
Mike WInfield <lolwut-lol@yandex.ru>
2023-11-11 01:20:22 +0300

    Typo & column name fixes

--- db/migrations/007_task.sql
+++ db/migrations/007_task.sql
@@ -5,16 +5,16 @@ CREATE TABLE IF NOT EXISTS public.task
     date_created timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     description text,
     name text NOT NULL DEFAULT 'Задача',
-    "start" timestamp without time zone,
-    "end" timestamp without time zone,
-    list_postition smallint NOT NULL,
+    "task_start" timestamp without time zone,
+    "task_end" timestamp without time zone,
+    list_position smallint NOT NULL,
     CONSTRAINT task_pkey PRIMARY KEY (id),
     CONSTRAINT task_id_list_fkey FOREIGN KEY (id_list)
         REFERENCES public.list (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
-    CONSTRAINT task_list_postition_check CHECK (list_postition >= 0) NOT VALID,
+    CONSTRAINT task_list_position_check CHECK (list_position >= 0) NOT VALID,
     CONSTRAINT task_name_length_check CHECK (length(name) <= 150) NOT VALID
 )
 
