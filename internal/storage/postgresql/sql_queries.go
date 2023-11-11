e1989fb92dc09af3d8a54aeb4d5aee6503ac3ddc
Mike WInfield <lolwut-lol@yandex.ru>
2023-11-11 03:56:50 +0300

    Comment out unused query

--- internal/storage/postgresql/sql_queries.go
+++ internal/storage/postgresql/sql_queries.go
@@ -12,11 +12,11 @@ var (
 	newTaskFields    = []string{"id_list", "name", "description", "list_position", "task_start", "task_end"}
 	allSessionFields = []string{"id_user", "expiration_date"}
 	//allBoardFields             = []string{"id", "id_workspace", "name", "description", "date_created", "thumbnail_url"}
-	allTaskAndUserFields = []string{
-		"public.task.id", "public.task.id_list", "public.task.date_created", "public.task.name",
-		"public.task.description", "public.task.list_position", "public.task.task_start", "public.task.task_end",
-		"public.user.id", "public.user.email", "public.user.password_hash", "public.user.name", "public.user.surname", "public.user.avatar_url", "public.user.description",
-	}
+	// allTaskAndUserFields = []string{
+	// 	"public.task.id", "public.task.id_list", "public.task.date_created", "public.task.name",
+	// 	"public.task.description", "public.task.list_position", "public.task.task_start", "public.task.task_end",
+	// 	"public.user.id", "public.user.email", "public.user.password_hash", "public.user.name", "public.user.surname", "public.user.avatar_url", "public.user.description",
+	// }
 	allWorkspaceAndBoardFields = []string{
 		"public.workspace.id", "public.workspace.name", "public.workspace.description", "public.workspace.date_created", "public.workspace.thumbnail_url",
 		"public.boards.id", "public.boards.name", "public.boards.description", "public.boards.date_created", "public.boards.thumbnail_url",
