591c1e17950f067838bfbdeef448a540c0f0f9eb
Mike WInfield <lolwut-lol@yandex.ru>
2023-11-12 20:19:21 +0300

    CSRF improved error logging

--- internal/app/middleware/csrf.go
+++ internal/app/middleware/csrf.go
@@ -14,9 +14,15 @@ func CSRFMiddleware(cs service.ICSRFService) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			rCtx := r.Context()
 			log.Println("Verifying CSRF")
-			err := cs.VerifyCSRF(rCtx, dto.CSRFToken{Value: r.Header.Get("X-CSRF-Token")})
-			if err != nil {
+			csrf := r.Header.Get("X-CSRF-Token")
+			if csrf == "" {
 				log.Println("CSRF header not set on incoming request")
+				*r = *r.WithContext(context.WithValue(rCtx, dto.ErrorKey, apperrors.GenericUnauthorizedResponse))
+				return
+			}
+			err := cs.VerifyCSRF(rCtx, dto.CSRFToken{Value: csrf})
+			if err != nil {
+				log.Println("CSRF invalid")
 				*r = *r.WithContext(context.WithValue(rCtx, dto.ErrorKey, apperrors.GenericUnauthorizedResponse))
 				return
 			}
