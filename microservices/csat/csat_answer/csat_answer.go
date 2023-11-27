d2113c809f5e54a32fd3731d96cdc5617e04eaa6
Mike WInfield <lolwut-lol@yandex.ru>
2023-11-27 22:51:55 +0300

    Restructure further

---
+++ microservices/csat/csat_answer/csat_answer.go
@@ -0,0 +1,34 @@
+package csat_microservice
+
+import (
+	context "context"
+	"server/internal/pkg/dto"
+	"server/internal/storage"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type CSATAnswerService struct {
+	storage storage.ICSATAnswerStorage
+	UnimplementedCSATAnswerServiceServer
+}
+
+// NewBoardService
+// возвращает BoardService с инициализированным хранилищем
+func NewCSATAnswerService(storage storage.ICSATAnswerStorage) *CSATAnswerService {
+	return &CSATAnswerService{
+		storage: storage,
+	}
+}
+
+// Create
+// создает новый ответ CSAT
+// или возвращает ошибки ...
+func (cs CSATAnswerService) Create(ctx context.Context, info *NewCSATAnswer) (*emptypb.Empty, error) {
+	convertedInfo := dto.NewCSATAnswer{
+		UserID:     info.UserID,
+		QuestionID: info.QuestionID,
+		Rating:     info.Rating,
+	}
+	return nil, cs.storage.Create(ctx, convertedInfo)
+}
