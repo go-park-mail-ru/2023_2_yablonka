42ed488fa62c814626b5222953f70901e943c12e
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2023-11-29 23:12:34 +0300

    Тест конфига

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,6 +1,7 @@
 package config_test
 
 import (
+	"os"
 	"server/internal/apperrors"
 	"server/internal/config"
 	"testing"
@@ -14,31 +15,52 @@ func Test_SessionConfig(t *testing.T) {
 
 	tests := []struct {
 		name          string
-		config        config.SessionConfig
-		serviceType   string
+		configObj     config.SessionConfig
+		envVaribles   map[string]string
 		expectedError error
 	}{
 		{
 			name: "Valid config",
-			config: config.SessionConfig{
+			configObj: config.SessionConfig{
 				Duration: time.Duration(30 * 24 * time.Hour),
 				IDLength: 32,
+			},
+			envVaribles: map[string]string{
+				"SESSION_DURATION_DAYS":    "14",
+				"SESSION_DURATION_HOURS":   "0",
+				"SESSION_DURATION_MINUTES": "0",
+				"SESSION_DURATION_SECONDS": "0",
+				"SESSION_ID_LENGTH":        "32",
 			},
 			expectedError: nil,
 		},
 		{
 			name: "Invalid config (Session duration is 0)",
-			config: config.SessionConfig{
+			configObj: config.SessionConfig{
 				Duration: time.Duration(0 * time.Second),
 				IDLength: 32,
+			},
+			envVaribles: map[string]string{
+				"SESSION_DURATION_DAYS":    "0",
+				"SESSION_DURATION_HOURS":   "0",
+				"SESSION_DURATION_MINUTES": "0",
+				"SESSION_DURATION_SECONDS": "0",
+				"SESSION_ID_LENGTH":        "32",
 			},
 			expectedError: apperrors.ErrSessionNullDuration,
 		},
 		{
 			name: "Invalid config (Session ID length is 0)",
-			config: config.SessionConfig{
+			configObj: config.SessionConfig{
 				Duration: time.Duration(30 * 24 * time.Hour),
 				IDLength: 0,
+			},
+			envVaribles: map[string]string{
+				"SESSION_DURATION_DAYS":    "14",
+				"SESSION_DURATION_HOURS":   "0",
+				"SESSION_DURATION_MINUTES": "0",
+				"SESSION_DURATION_SECONDS": "0",
+				"SESSION_ID_LENGTH":        "0",
 			},
 			expectedError: apperrors.ErrSessionNullIDLength,
 		},
@@ -47,6 +69,9 @@ func Test_SessionConfig(t *testing.T) {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+			for key, value := range test.envVaribles {
+				os.Setenv(key, value)
+			}
 			_, err := config.NewSessionConfig()
 
 			require.ErrorIs(t, err, test.expectedError)
