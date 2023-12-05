2ce3ea80d6a4ca42097c3cd6e4d4999a82681704
Mike WInfield <lolwut-lol@yandex.ru>
2023-12-05 14:52:18 +0300

    Delete unused service implementations

--- internal/service/csrf/csrf.go
+++ internal/service/csrf/csrf.go
@@ -4,15 +4,10 @@ import (
 	"server/internal/config"
 	"server/internal/storage"
 
-	embedded "server/internal/service/csrf/embedded"
 	micro "server/internal/service/csrf/microservice"
 
 	"google.golang.org/grpc"
 )
-
-func NewEmbeddedCSRFService(csrfStorage storage.ICSRFStorage, config config.SessionConfig) *embedded.CSRFService {
-	return embedded.NewCSRFService(config, csrfStorage)
-}
 
 // TODO: CSRF microservice
 func NewMicroCSRFService(csrfStorage storage.ICSRFStorage, config config.SessionConfig, connection *grpc.ClientConn) *micro.CSRFService {
