cfdc6d56404647cea5b18cf6eaf9cdedeab46566
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2023-12-24 17:17:57 +0300

    Удалить constraint тэга

--- db/migrations/public/006_tag.sql
+++ db/migrations/public/006_tag.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS public.tag
     name text NOT NULL,
     color text NOT NULL DEFAULT 'FFFFFF',
     CONSTRAINT tag_pkey PRIMARY KEY (id),
-    CONSTRAINT tag_name_id_name1_id1_key UNIQUE (name),
     CONSTRAINT tag_color_length_check CHECK (length(color) <= 6) NOT VALID,
     CONSTRAINT tag_name_length_check CHECK (length(name) <= 35) NOT VALID
 )
